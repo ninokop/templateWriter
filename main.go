17e2e71658651a39ecff3460f403c068413cebc3
z00349883 <zhoutian2@huawei.com>
2017-11-17 17:02:48 +0800

    init template

---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	// "os"
+	text_template "text/template"
+)
+
+type Metrics struct {
+	PluginID      string
+	MetricsSource string
+	ScopeName     string
+	TimeStamp     string
+	InfaceName    string
+	MetricsKey    string
+	MetricsValue  string
+}
+
+func main() {
+	tmpl := text_template.New("metrics.dat")
+	t, err := tmpl.ParseFiles("./metrics.dat")
+	if err != nil {
+		fmt.Printf("Parse template file error: %v\n", err)
+		return
+	}
+	m := Metrics{
+		PluginID:      "CSE_ServiceCenter",
+		MetricsSource: "10.120.195.2",
+		ScopeName:     "service_center",
+		InfaceName:    "inface_name",
+		MetricsKey:    "cpu_total",
+	}
+
+	buffer := new(bytes.Buffer)
+	if err := t.Execute(buffer, m); err != nil {
+		fmt.Printf("Execute get template error: %v\n", err)
+		return
+	}
+	fmt.Printf("Content Files is: %s\n", string(buffer.Bytes()))
+}
